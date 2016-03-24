3473af7127f29d45b71e920d449a77d25e91b2f7
Chris White <white.cdw@gmail.com>
2016-03-23 22:34:57 -0400

    Debuggin a pip error.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+#def read(fname):
+   # return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "White Morgan Capital Management",
@@ -18,7 +18,7 @@ setup(
     keywords = "",
     url = "",
     packages=['wmcm', 'tests'],
-    long_description=read('README'),
+    #long_description=read('README'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
