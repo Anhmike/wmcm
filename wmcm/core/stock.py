17ea677a10de2edbcdeb6b102659467a0101de33
camelCake <daniel.freetime@gmail.com>
2016-04-29 00:48:59 -0500

    goodnight, moon

--- wmcm/core/stock.py
+++ wmcm/core/stock.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import datetime as dt
 import pandas as pd
 from pandas_datareader import data
+import wmcm.functions as wmf
 
 class stock(object):
     '''Core Stock Class
@@ -10,6 +11,9 @@ class stock(object):
     tic : string containing Ticker symbol
     start : (string) beginning date of analysis period, in the format '%Y-%m-%d' (e.g. '2010-01-01')
     end : (string) ending date of analysis period, in the format '%Y-%m-%d' (e.g. '2012-12-31')
+    interval : string, default 'd'
+        Time interval code, valid values are 'd' for daily, 'w' for weekly,
+        'm' for monthly and 'v' for dividend.
     '''
 
     ## just for the learning experience, creating a "read-only" ticker property
@@ -34,7 +38,7 @@ class stock(object):
     def bench(self, tic, start, end):
         self._bench = self.bench(tic, start, end)
 
-    def __init__(self, tic, start='2010-01-01', end='2012-09-20'):
+    def __init__(self, tic, start='2010-01-01', end='2012-09-20', interval='d'):
         if tic is None:
             tic='SPY'
         assert type(tic)==str, 'Ticker must be a string.'
@@ -42,7 +46,9 @@ class stock(object):
         self.ticker = tic.strip()
         self.start = dt.datetime.strptime(start, '%Y-%m-%d')
         self.end = dt.datetime.strptime(end, '%Y-%m-%d')
-        self.data = data.DataReader(tic, data_source='yahoo', start=start, end=end)
+        self.raw = data.YahooDailyReader(tic, start = start, end = end, interval = interval).read()
+        # self.adj = wmf.adjust_prices(self.raw)
+        # self.returns = wmf.add_returns(self.adj)
 
     def __getitem__(self,key):
         return self.data[key]
--- wmcm/functions.py
+++ wmcm/functions.py
@@ -1,2 +1,27 @@
+import datetime as dt
+import pandas as pd
+import numpy as np
+from pandas_datareader import data
+
+
 def greg():
  print('Fuck off!')
+
+def adjust_prices(df):
+    '''Adjusts df prices for dividends and splits.'''
+    df['AdjFactor'] = df['Adj Close'].shift(1) / df['Close'].shift(1)
+    df['Open'] = df['Open'] * df['AdjFactor']
+    df['High'] = df['High'] * df['AdjFactor']
+    df['Low'] = df['Low'] * df['AdjFactor']
+    df['Close'] = df['Close'] * df['AdjFactor']
+    new_df = df[['sector', 'symbol', 'Open', 'High', 'Low', 'Close', 'Volume']]
+    new_df.columns = ['sector', 'symbol', 'open', 'high', 'low', 'close', 'volume']
+    return new_df
+
+def add_returns(df):
+    df['lastClose'] = df['close'].shift(1)
+    df['ret_cc'] = np.log(df['close'] / df['lastClose'])
+    df['ret_oc'] = np.log(df['close'] / df['open'])
+    df['ret_co'] = np.log(df['open'] / df['lastClose'])
+    df.drop('lastClose', 1, inplace=True)
+    return df
