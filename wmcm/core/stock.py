6e30a984ef7e5298ef36393eaaeca4e8b66db40b
camelCake <daniel.freetime@gmail.com>
2016-04-30 00:35:46 -0500

    Continued working on stock class.

--- wmcm/core/stock.py
+++ wmcm/core/stock.py
@@ -30,54 +30,50 @@ class stock(object):
             self._ticker = value
 
     # I'm not fucking with that @property shit. I try to keep it real, n-word.
-    def prices.get_raw(self, interval = 'm', start, end):
-        history = data.YahooDailyReader(self._ticker, start = start, end = end, interval = interval)
+    def get_raw_prices(self, start, end, interval = 'm'):
+        history = data.YahooDailyReader(self.ticker, start = start, end = end, interval = interval)
         # Need to add assertion here that interval is m, w, d or v.
-        if interval = 'm':
+        if interval == 'm':
             self.prices.raw.m = history.read()
-        elif interval = 'w':
+        elif interval == 'w':
             self.prices.raw.w = history.read()
-        elif interval = 'd':
+        elif interval == 'd':
             self.prices.raw.d = history.read()
-        elif interval = 'v':
+        elif interval == 'v':
             self.prices.raw.v = history.read()
 
-    def prices.get_adj(self, interval = 'm'):
+    def get_adj_prices(self, interval = 'm'):
         # Need to add assertion here that interval is m, w, d or v.
-        if interval = 'm':
+        if interval == 'm':
             self.prices.adj.m = wmf.adjust_prices(self.prices.raw.m)
-        elif interval = 'w':
+        elif interval == 'w':
             self.prices.adj.w = wmf.adjust_prices(self.prices.raw.w)
-        elif interval = 'd':
+        elif interval == 'd':
             self.prices.adj.d = wmf.adjust_prices(self.prices.raw.d)
-        elif interval = 'v':
+        elif interval == 'v':
             self.prices.adj.v = wmf.adjust_prices(self.prices.raw.v)
     
     def returns_get(self, interval = 'm'):
         # Need to add assertion here that interval is m, w, d or v.
-        if interval = 'm':
+        if interval == 'm':
             self.returns.m = wmf.get_returns(self.prices.adj.m)
-        elif interval = 'w':
+        elif interval == 'w':
             self.returns.w = wmf.get_returns(self.prices.adj.w)
-        elif interval = 'd':
+        elif interval == 'd':
             self.returns.d = wmf.get_returns(self.prices.adj.d)
-        elif interval = 'v':
+        elif interval == 'v':
             self.returns.v = wmf.get_returns(self.prices.adj.v)
 
 
-    def __init__(self, tic, start='2010-01-01', end='201-12-31', interval='m'):
-        if tic is None:
-            tic='SPY'
-        assert type(tic)==str, 'Ticker must be a string.'
- 
-        self.ticker = tic.strip()
+    def __init__(self, tic, start='2010-01-01', end='2015-12-31', interval='m'):
+        self.ticker = tic
         self.start = dt.datetime.strptime(start, '%Y-%m-%d')
         self.end = dt.datetime.strptime(end, '%Y-%m-%d')
-        self.prices.get_raw(self, start=start, end=end, interval=interval)
-        self.prices.get_adj(self, interval=interval)
+        self.get_raw_prices(self, start=start, end=end, interval=interval)
+        self.get_adj_prices(self, interval=interval)
         self.returns_get(self, interval=interval)
 
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         return self.data[key]
 
     def __repr__(self):
