1ca5fc7fb05d21f53d3749410eeb7d774ee1ceb2
Chris White <white.cdw@gmail.com>
2016-05-01 11:50:08 -0400

    Updated functions.py with some comments and making deep copies of passed DataFrames.

--- wmcm/functions.py
+++ wmcm/functions.py
@@ -3,25 +3,36 @@ import pandas as pd
 import numpy as np
 from pandas_datareader import data
 
-
-def greg():
- print('Fuck off!')
-
 def adjust_prices(df):
     '''Adjusts df prices for dividends and splits.'''
-    df['AdjFactor'] = df['Adj Close'].shift(1) / df['Close'].shift(1)
-    df['Open'] = df['Open'] * df['AdjFactor']
-    df['High'] = df['High'] * df['AdjFactor']
-    df['Low'] = df['Low'] * df['AdjFactor']
-    df['Close'] = df['Close'] * df['AdjFactor']
-    new_df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
+
+    ## make a deep copy of the input data so we don't accidentally chang it
+    data = df.copy()
+
+    ## What is the rationale behind this? Comment this up a bit so I don't edit the intended purpose
+    data['AdjFactor'] = data['Adj Close'].shift(1) / data['Close'].shift(1)
+    data['Open'] = data['Open'] * data['AdjFactor']
+    data['High'] = data['High'] * data['AdjFactor']
+    data['Low'] = data['Low'] * data['AdjFactor']
+    data['Close'] = data['Close'] * data['AdjFactor']
+
+    new_df = data[['Open', 'High', 'Low', 'Close', 'Volume']]
     new_df.columns = ['open', 'high', 'low', 'close', 'volume']
+
     return new_df
 
 def get_returns(df):
-    df['lastClose'] = df['close'].shift(1)
-    df['ret_cc'] = np.log(df['close'] / df['lastClose'])
-    df['ret_oc'] = np.log(df['close'] / df['open'])
-    df['ret_co'] = np.log(df['open'] / df['lastClose'])
-    df.drop('lastClose', 1, inplace=True)
-    return df
+    '''Gets returns...'''
+
+    ## make a deep copy of the input data so we don't accidentally chang it
+    data = df.copy()
+
+    data['lastClose'] = data['close'].shift(1)
+    data['ret_cc'] = np.log(data['close'] / data['lastClose'])
+    data['ret_oc'] = np.log(data['close'] / data['open'])
+    data['ret_co'] = np.log(data['open'] / data['lastClose'])
+
+    ## what is this for?
+    data.drop('lastClose', 1, inplace=True)
+
+    return data
